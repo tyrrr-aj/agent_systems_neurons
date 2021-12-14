01c015b3f6e8cbfbec95d3334cf9a1d55fd24dac
Benedykt Roszko <JXWG36@motorolasolutions.com>
2021-12-14 20:46:12 +0100

    base agent impl

---
+++ as_project/src/main/java/app/Constants.java
@@ -0,0 +1,8 @@
+package app;
+
+public class Constants {
+    public static final double ACTIVATION_TIME = 1;
+    public static final double REFRACTION_TIME = 2;
+    public static final double THRESHOLD = 1.0;
+    public static final double RELAXATION_RATIO = -0.1;
+}
