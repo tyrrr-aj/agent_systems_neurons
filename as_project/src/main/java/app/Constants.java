e181397ed42da6ba9b207db4c2db6f206a37e867
Adam Szreter <adam.szreter@op.pl>
2022-01-17 00:50:44 +0100

    Accuracy 0.78, many neurons

--- as_project/src/main/java/app/Constants.java
+++ as_project/src/main/java/app/Constants.java
@@ -1,12 +1,12 @@
 package app;
 
 public class Constants {
-    public static final double ACTIVATION_TIME = 1.0;
+    public static final double ACTIVATION_TIME = 1.5;
     public static final double REFRACTION_TIME = 1.0;
     public static final double THRESHOLD = 1.0;
     public static final double RELAXATION_RATIO = 0.05; // must be positive
     public static final double OBJECT_NODE_CHECK_TIME = 0.0;
-    public static final double EPSILON = 2.5;
-    public static final double RECORD_STIMULATION_TIME = 1.5;
-    public static final double PAUSE_BETWEEN_RECORDS = 3.0;
+    public static final double EPSILON = 2.0;
+    public static final double RECORD_STIMULATION_TIME = 1.3;
+    public static final double PAUSE_BETWEEN_RECORDS = 8.0;
 }
