ccc6e976596056f31b7edfe358163d4cd7cfcb9c
Adam Szreter <adam.szreter@op.pl>
2022-01-17 00:29:32 +0100

    Accuracy 0.78

--- as_project/src/main/java/app/Constants.java
+++ as_project/src/main/java/app/Constants.java
@@ -5,7 +5,7 @@ public class Constants {
     public static final double REFRACTION_TIME = 1.0;
     public static final double THRESHOLD = 1.0;
     public static final double RELAXATION_RATIO = 0.05; // must be positive
-    public static final double OBJECT_NODE_CHECK_TIME = 0.4;
+    public static final double OBJECT_NODE_CHECK_TIME = 0.5;
     public static final double EPSILON = 2.5;
     public static final double RECORD_STIMULATION_TIME = 2.0;
     public static final double PAUSE_BETWEEN_RECORDS = 3.0;
